3b622ca303cfda8885c268140c0bca9bc0815ecb
1F10230069 <s1F102300694@iniad.org>
2024-07-11 22:40:29 +0900

    最終調整

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, redirect
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
@@ -29,7 +29,7 @@ def detail(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    
+
     context = {
         'task': task,
     }
@@ -41,12 +41,12 @@ def update(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    if request.method =="POST":
+    if request.method == "POST":
         task.title = request.POST['title']
-        task,due_at = make_aware(parse_datetime(request.POST["due_at"]))
+        task, due_at = make_aware(parse_datetime(request.POST["due_at"]))
         task.save()
         return redirect(detail, task_id)
-    
+
     context = {
         'task': task
     }
