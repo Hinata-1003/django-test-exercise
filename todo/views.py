5d121208d461f9986f5799863097aac4f958d26e
s1F10220209 <s1f102202099@iniad.org>
2024-07-12 09:27:56 +0900

    正しく動くように修正

--- todo/views.py
+++ todo/views.py
@@ -43,14 +43,14 @@ def update(request, task_id):
         raise Http404("Task does not exist")
     if request.method =="POST":
         task.title = request.POST['title']
-        task,due_at = make_aware(parse_datetime(request.POST["due_at"]))
+        task.due_at = make_aware(parse_datetime(request.POST["due_at"]))
         task.save()
         return redirect(detail, task_id)
     
     context = {
         'task': task
     }
-    return render(request, "todo/edhit.html", context)
+    return render(request, "todo/edit.html", context)
 
 
 def delete(request, task_id):
